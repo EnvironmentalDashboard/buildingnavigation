01ea5cb276ff1e4c9e8a42faed0e4d3804ee8896
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-02-28 12:02:59 -0500

    close icon

--- buildingosnavigation/currentelecfilteros.php
+++ buildingosnavigation/currentelecfilteros.php
@@ -10,7 +10,7 @@ require '../../includes/class.TimeSeries.php';
   <meta charset="UTF-8">
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../css/bootstrap.grid.css">
+  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
   <link rel="stylesheet" href="buildosnavstyle.css">
